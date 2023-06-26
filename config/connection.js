f2170a791482e70685fbe003b5a34d2a7c2e4a1f
Lendo <wesreedbusiness@gmail.com>
2023-06-25 22:44:22 -0500

    config

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
 const Sequelize = require('sequelize');
-const config = require("./config");
+require('dotenv').config();
 
 let sequelize;
 
-if (config.production.use_env_variable) {
-  sequelize = new Sequelize(config.production.use_env_variable);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    config.development.database,
-    config.development.username,
-    config.development.password,
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'mysql',
