ee18663d28f3ae727f27db69093a8ffa48cb3e28
Lendo <wesreedbusiness@gmail.com>
2023-06-26 18:14:34 -0500

    get all api route del

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,18 +2,6 @@ const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', async  (req,res) =>{
-  try{
-    const postData = await Post.findAll({})
-    const posts = postData.map((post) => post.get({ plain: true }));
-
-    res.status(200).json(posts)
-  } catch (err){
-    res.status(500).json(err)
-  }
- })
-
-
 router.post('/', withAuth, async (req, res) => {
   try {
     const newPost = await Post.create({
