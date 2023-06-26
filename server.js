9eeed9951d95004e8b0a15effc5771a21bb4c86e
Lendo <wesreedbusiness@gmail.com>
2023-06-25 21:05:44 -0500

    data reset

--- server.js
+++ server.js
@@ -44,17 +44,3 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-
-
-
-/*
-WHEN I click on an existing blog post
-THEN I am presented with the post title, contents, post creator’s username, and date created for that post and have the option to leave a comment
-
-WHEN I enter a comment and click on the submit button while signed in
-THEN the comment is saved and the post is updated to display the comment, the comment creator’s username, and the date created
-
-WHEN I click on the button to add a new blog post
-THEN I am prompted to enter both a title and contents for my blog post
-
-*/
